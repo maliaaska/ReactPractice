ced6ad8f471f52b137258fb18c87d29a57e691b3
Arkadiusz <asmilewski@gmail.com>
2017-08-24 16:44:51 +0200

    React first component

--- public/app.jsx
+++ public/app.jsx
@@ -1,5 +1,17 @@
 
-        ReactDOM.render(
-            <h1>Hello React!</h1>,
-            document.getElementById('app')    
+var Greeter = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>Hello React!</h1>
+                <p>This is form a component!</p>
+            </div>
         );
+    }
+})
+
+ 
+ReactDOM.render(
+    <Greeter/>,
+    document.getElementById('app')    
+);
