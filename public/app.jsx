faa7dcf31e034809d05718998fcd53a1784f7dd5
Arkadiusz <asmilewski@gmail.com>
2017-08-25 15:35:08 +0200

    User event callbacks

--- public/app.jsx
+++ public/app.jsx
@@ -6,6 +6,17 @@ var Greeter = React.createClass({
             message: "This is from the component"
         }
     },
+
+    onButtonClick: function (e){
+
+        //This event prevents refreshing page
+        e.preventDefault();
+        
+        var name = this.refs.name.value;
+
+        alert(name);
+
+    },
     render: function () {
         var name = this.props.name;
         var message = this.props.message;
@@ -13,6 +24,12 @@ var Greeter = React.createClass({
             <div>
                 <h1>Hello {name}!</h1>
                 <p>{message}</p>
+
+
+                <form onSubmit={this.onButtonClick}>
+                    <input type="text" ref="name"/>
+                    <button>Set Name</button>
+                </form>
             </div>
         );
     }
