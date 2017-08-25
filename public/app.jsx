5b932a8df27284dcab15a0683009b40f902e7eac
Arkadiusz <asmilewski@gmail.com>
2017-08-25 16:26:28 +0200

    nested components complete

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,27 @@
 
+var GreeterMessage = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1> Some H1</h1>
+                <p>Some paragraph</p>
+        </div>
+        );
+    }
+});
+
+var GreeterForm = React.createClass({
+    render: function () {
+        return (
+            <form>
+                <input type="text" ref="name"/>
+                <button>Set Name</button>
+            </form>
+
+        );
+    }
+})
+
 var Greeter = React.createClass({
     getDefaultProps: function() {
         return {
@@ -39,11 +62,14 @@ var Greeter = React.createClass({
                 <h1>Hello {name}!</h1>
                 <p>{message}</p>
 
+                <GreeterMessage/>
+
 
                 <form onSubmit={this.onButtonClick}>
                     <input type="text" ref="name"/>
                     <button>Set Name</button>
                 </form>
+                <GreeterForm/>
             </div>
         );
     }
