94cb9be0643f97c800ab1c91acd6f37eecd70961
Arkadiusz <asmilewski@gmail.com>
2017-08-25 15:21:01 +0200

    Component properties done properly

--- public/app.jsx
+++ public/app.jsx
@@ -1,17 +1,27 @@
 
 var Greeter = React.createClass({
+    getDefaultProps: function() {
+        return {
+            name: "React",
+            message: "This is from the component"
+        }
+    },
     render: function () {
+        var name = this.props.name;
+        var message = this.props.message;
         return (
             <div>
-                <h1>Hello React!</h1>
-                <p>This is form a component!</p>
+                <h1>Hello {name}!</h1>
+                <p>{message}</p>
             </div>
         );
     }
 })
 
- 
+var firstName = 'Arkadiusz';
+var firstMessage = 'Yeah this is from the component but the second message'
 ReactDOM.render(
-    <Greeter/>,
+    <Greeter name={firstName} message={firstMessage} />,
+    
     document.getElementById('app')    
 );
