b6c81ced720fa19cbdb060f150aba89dc3aea439
Arkadiusz <asmilewski@gmail.com>
2017-08-26 12:07:53 +0200

    adding babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,28 @@
 
 
  // 1. entry property tells webpack where it should start proccessing your code
-module.exports = {
-    entry: './public/app.js',
+ // 2. we telling babel-loader to take our files parse them through react
+ // get the output and run them through es2015 as well
+ module.exports = {
+    entry: './public/app.jsx',
     output: {
         path: __dirname,
         filename: './public/bundle.js'
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+
     }
 };
